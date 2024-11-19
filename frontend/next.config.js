d0860c38d95e36450ddb5a7893d1be81e23f6a11
David Marx <david.marx84@gmail.com>
2024-11-19 01:01:05 -0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.ya?ml$/,
-      use: 'yaml-loader',
+      use: 'raw-loader'
     });
     return config;
   },

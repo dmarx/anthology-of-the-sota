61f6942e56b4c152c52b2f9fdc06c0728e4a84ab
David Marx <david.marx84@gmail.com>
2024-11-19 10:11:11 -0800

    Create 404.tsx

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <div className="min-h-screen p-4">
+      <div className="max-w-7xl mx-auto p-4 bg-red-50 border border-red-200 rounded">
+        <h1 className="text-red-800 text-xl font-bold">404 - Page Not Found</h1>
+        <p className="text-red-600">The requested page could not be found.</p>
+      </div>
+    </div>
+  )
+}

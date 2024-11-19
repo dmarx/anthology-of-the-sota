41563a9594a5e82c3972ccf231002721b5a1e64a
David Marx <david.marx84@gmail.com>
2024-11-19 13:37:26 -0800

    Update _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// frontend/src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="min-h-screen bg-gray-50">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
